8100bb4a4fb4c9b9386838fd66fdbd6bcec88292
Gabriel Leal <gabriel.lealsantos@hotmail.com>
2026-07-14 10:17:04 -0300

    Atualizando o frontend

--- frontend/src/routes/criar-avatar.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AvatarStudio } from "@/features/avatars/avatar-studio";
-
-export const Route = createFileRoute("/criar-avatar")({
-  component: AvatarStudio,
-});
--- frontend/src/routes/modelos.index.tsx
+++
@@ -1,4 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { ModelosScreen } from "@/features/modelos/modelos-screen";
-
-export const Route = createFileRoute("/modelos/")({ component: ModelosScreen });
